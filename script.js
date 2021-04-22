ff6f3759a9cbafdb00155a833b5ce325e5b57fec
AlessandroSestili <sestilialessandro@gmail.com>
2021-04-22 17:45:54 +0200

    aggiungo log

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ var studente = {
 //Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 stampaProprietàOggetto(studente);
 
-
 // Creare un array di oggetti di studenti.
 var classe33 = [{
     nome: "Maurizio",
@@ -34,15 +33,20 @@ for (var i = 0; i < classe33.length; i++) {
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 classe33.push({});
 
+// Chiedo all'utente
 var nomeUtente = prompt("Inserisci il tuo nome: ")
 var cognomeUtente = prompt("Inserisci il tuo cognome: ")
 var etàUtente = prompt("Inserisci la tua età: ")
 
+// Aggiungo all'Array classe33 i dati inseriti dall'Utente
 classe33[3] = {
     'nome': nomeUtente,
     'cognome': cognomeUtente,
     'età': etàUtente
 }
+
+console.log(classe33);
+console.log(classe33[3]);
 
 
 
